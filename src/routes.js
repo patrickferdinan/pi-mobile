8d31b25180e9ef6996129dc890ea05d628be521f
Patrick Ferdinan <patrick.ferdinan@hotmail.com>
2020-05-03 19:23:21 -0300

    chore: estilo pagina inicial

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 
 const AppStack = createStackNavigator();
-
-import Logon from './pages/Logon';
 import Event from './pages/Event';
+import Logon from './pages/Logon';
 import Perfil from './pages/Perfil';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
@@ -17,8 +16,8 @@ export default function Routes() {
         <NavigationContainer>
 
             <AppStack.Navigator screenOptions={{headerShown: false }}>
-                <AppStack.Screen name="Logon" component={Logon} />
                 <AppStack.Screen name="Event" component={Event} />
+                <AppStack.Screen name="Logon" component={Logon} /> 
                 <AppStack.Screen name="Perfil" component={Perfil} />
                 <AppStack.Screen name="Profile" component={Profile} />
                 <AppStack.Screen name="Register" component={Register} />
